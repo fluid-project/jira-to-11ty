7a136d06dc9dc060d298f52639810887619ed0ff
Justin Obara <556024+jobara@users.noreply.github.com>
2025-04-10 10:21:36 -0400

    feat: pull in all projects from JIRA (#1)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,11 @@
 import MarkdownIt from "markdown-it";
-import Shiki from "@shikijs/markdown-it";
+import highlightjs from "markdown-it-highlightjs";
 import { fromADF } from "mdast-util-from-adf";
 import { toMarkdown } from "mdast-util-to-markdown";
 import { gfmTableToMarkdown } from "mdast-util-gfm-table";
+import { gfmStrikethroughToMarkdown } from 'mdast-util-gfm-strikethrough';
+import parseTransform from "./src/_transforms/parse.js";
+import * as pagefind from "pagefind";
 
 const md = new MarkdownIt({
   html: true,
@@ -10,27 +13,59 @@ const md = new MarkdownIt({
   typographer: true,
 });
 
-md.use(
-  await Shiki({
-    themes: {
-      light: "github-light",
-      dark: "github-dark",
-    },
-  }),
-);
+md.use(highlightjs);
 
 export default function eleventy(eleventyConfig) {
   eleventyConfig.addFilter("adf", (value) => {
     return md.render(
-      toMarkdown(fromADF(value), { extensions: [gfmTableToMarkdown()] }),
+      toMarkdown(fromADF(value), {extensions: [gfmTableToMarkdown(), gfmStrikethroughToMarkdown()] }),
     );
+  })
+
+  // based on https://lea.verou.me/blog/2023/11ty-indices/#dynamic-postsbytag-collection
+  eleventyConfig.addCollection("projects", (collectionsApi) => {
+    // fetch a page so that we can access the global data through it.
+    const page = collectionsApi.getFilteredByGlob("src/index.njk");
+
+    const ret = {};
+
+    for (const issue of page[0].data.issues ?? []) {
+      const project = issue.fields?.project?.name;
+      if (project) {
+        ret[project] ??= [];
+        ret[project].push(issue);
+      }
+    }
+
+    return Object.fromEntries(Object.entries(ret).sort());
+  });
+
+  eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico"});
+  eleventyConfig.addPassthroughCopy({ "node_modules/@github/relative-time-element/dist": "assets/scripts/relative-time/"});
+  eleventyConfig.addPassthroughCopy({ "node_modules/@zachleat/filter-container/filter-container.js": "assets/scripts/filter-container.js"});
+  eleventyConfig.addPassthroughCopy({ "node_modules/@lowlighter/matcha/dist/matcha.css": "assets/styles/matcha.css"});
+  
+  eleventyConfig.addTransform("parse", parseTransform);
+
+  eleventyConfig.on("afterBuild", async () => {
+    // Create a Pagefind search index to work with
+    const { index } = await pagefind.createIndex();
+
+    // Index all HTML files in a directory
+    await index.addDirectory({
+      path: "_site"
+    });
+
+    // Write the index to disk
+    await index.writeFiles({
+      outputPath: "_site/pagefind"
+    });
   });
 
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      layouts: "_layouts",
       data: "_data",
     },
     htmlTemplateEngine: "njk",
--- src/_data/issues.js
+++ src/_data/issues.js
@@ -1,52 +1,66 @@
 import { AssetCache } from "@11ty/eleventy-fetch";
-import axios from "axios";
-import rateLimit from "axios-rate-limit";
+import Bottleneck from "bottleneck";
 
-const http = rateLimit(axios.create(), {
-  maxRequests: 2,
-  perMilliseconds: 1000,
-  maxRPS: 2,
-});
+async function fetchIssues(limiter, nextPageToken) {
+  let data = {};
+  const url = "https://fluidproject.atlassian.net/rest/api/3/search/jql";
+  const params = {
+    jql: "created < now() order by created ASC",
+    fields: "*all",
+  };
 
-async function fetchIssues() {
-  const res = [];
-  let totalPages;
-  let page = 1;
-  let startAt = page * 100 - 100;
+  if (nextPageToken) {
+    params["nextPageToken"] = nextPageToken;
+  }
 
-  do {
-    console.log(`Loading page ${page}…`);
+  const query = new URLSearchParams(params);
 
-    const { data } = await http.get(
-      `https://fluidproject.atlassian.net/rest/api/3/search?jql=project%3DFLUID&maxIssues=100&startAt=${startAt}`,
-    );
-    res.push(data.issues);
-    totalPages = Math.ceil(data.total / 100);
-    page++;
-    startAt = page * 100 - 100;
-  } while (page <= totalPages);
-  return res.flat();
+  limiter = limiter || new Bottleneck({
+    reservoir: 500,
+    reservoirRefreshAmount: 500,
+    reservoirRefreshInterval: 5 * 60 * 1000,
+    maxConcurrent: 1,
+    minTime: 100
+  });
+
+  try {
+    console.log(`Loading page ${url}?${query}`);
+    const response = await limiter.schedule(() => fetch(`${url}?${query}`, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "X-Force-Accept-Language": true,
+        "Accept-Language": "en"
+      }
+    }));
+
+    if (!response.status === "200") {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    data = await response.json();
+
+  } catch (error) {
+    throw error;
+  }
+
+  if (data.nextPageToken) {
+    let issues = await fetchIssues(limiter, data.nextPageToken);
+    return data.issues.concat(issues);
+  }
+
+  return data.issues;
 }
 
 export default async function () {
   const asset = new AssetCache("issues");
 
-  if (asset.isCacheValid("2h")) {
+  if (asset.isCacheValid("24h")) {
     return asset.getCachedValue();
   }
 
   const data = await fetchIssues();
-  // data.map(async (issue) => {
-  //   console.log(`Retrieving ${issue.key}…`);
 
-  //   const { issueData } = await http.get(
-  //     `https://fluidproject.atlassian.net/rest/api/3/issue/${issue.key}`,
-  //   );
-
-  //   issue.comments = issueData.fields.comment.comments;
-
-  //   return issue;
-  // });
   await asset.save(data, "json");
 
   return data;
