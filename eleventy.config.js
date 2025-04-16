01f0184d758e1bfd98ec152a680ac57a617d3711
Ned Zimmerman <ned@bight.dev>
2025-04-16 11:56:52 -0300

    feat: bundle and minimize CSS

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,16 +2,46 @@ import highlightjs from "markdown-it-highlightjs";
 import parseTransform from "./src/_transforms/parse.js";
 import * as pagefind from "pagefind";
 import { EleventyRenderPlugin } from "@11ty/eleventy";
+import postcss from "postcss";
+import atImport from "postcss-import";
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+import autoprefixer from "autoprefixer";
+import csso from 'postcss-csso';
 
 export default function eleventy(eleventyConfig) {
   eleventyConfig.addCollection("projects", (collectionsApi) => collectionsApi.getFilteredByGlob("src/projects/*.md"));
+
+  eleventyConfig.addTemplateFormats("css");
+  eleventyConfig.addExtension('css', {
+    outputFileExtension: 'css',
+    compile: async (content, path) => {
+      if (path !== './src/assets/styles/style.css') {
+        return;
+      }
+  
+      return async () => {
+        let output = await postcss([
+          atImport(),
+          autoprefixer(),
+          purgeCSSPlugin({
+            content: ['./src/**/*.njk'],
+            safelist: ['form', 'input', 'button']
+          }),
+          csso()
+        ]).process(content, {
+          from: path,
+        });
+  
+        return output.css;
+      }
+    }
+  });
 
   eleventyConfig.addPlugin(EleventyRenderPlugin);
 
   eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
   eleventyConfig.addPassthroughCopy({ "node_modules/@github/relative-time-element/dist": "assets/scripts/relative-time/" });
   eleventyConfig.addPassthroughCopy({ "node_modules/@zachleat/filter-container/filter-container.js": "assets/scripts/filter-container.js" });
-  eleventyConfig.addPassthroughCopy({ "node_modules/@lowlighter/matcha/dist/matcha.css": "assets/styles/matcha.css" });
 
   eleventyConfig.addTransform("parse", parseTransform);
 	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(highlightjs));
