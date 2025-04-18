ef5a2349bf6eb1f608bed6d709a78189b6e48c34
Ned Zimmerman <ned@bight.dev>
2025-04-18 00:44:15 +0000

    feat: bundle and minimize CSS (#12)
    * feat: bundle and minimize CSS
    
    * fix: move dependencies from devDependencies
    
    * fix: add fieldset to safelist
    
    * fix: some adjustments based on feedback
    
    Signed-off-by: Ned Zimmerman <ned@bight.dev>
    
    * fix: adjustments based on PR review
    
    Signed-off-by: Ned Zimmerman <ned@bight.dev>
    
    * fix: syntax highlighting
    
    * fix: rebuild package-lock.json
    
    Signed-off-by: Ned Zimmerman <ned@bight.dev>
    
    ---------
    
    Signed-off-by: Ned Zimmerman <ned@bight.dev>

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,19 +2,56 @@ import highlightjs from "markdown-it-highlightjs";
 import parseTransform from "./src/_transforms/parse.js";
 import * as pagefind from "pagefind";
 import { EleventyRenderPlugin } from "@11ty/eleventy";
+import postcss from "postcss";
+import atImport from "postcss-import";
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+import autoprefixer from "autoprefixer";
+import csso from 'postcss-csso';
 
 export default function eleventy(eleventyConfig) {
-  eleventyConfig.addCollection("projects", (collectionsApi) => collectionsApi.getFilteredByGlob("src/projects/*.md"));
+  eleventyConfig.addCollection("projects", (collectionsApi) => collectionsApi
+    .getFilteredByGlob("src/projects/*.md")
+    .sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title);
+    }
+  ));
+
+  eleventyConfig.addTemplateFormats("css");
+  eleventyConfig.addExtension('css', {
+    outputFileExtension: 'css',
+    compile: async (content, path) => {
+      if (path !== './src/assets/styles/style.css') {
+        return;
+      }
+
+      return async () => {
+        let output = await postcss([
+          atImport(),
+          autoprefixer(),
+          purgeCSSPlugin({
+            content: ['./src/**/*.njk'],
+            safelist: {
+              standard: ['form', 'input', '[type="checkbox"]', 'button', 'fieldset', 'legend', 'mark', 'code', 'pre', 'hljs-string', 'hljs'],
+            }
+          }),
+          csso()
+        ]).process(content, {
+          from: path,
+        });
+
+        return output.css;
+      }
+    }
+  });
 
   eleventyConfig.addPlugin(EleventyRenderPlugin);
 
   eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
   eleventyConfig.addPassthroughCopy({ "node_modules/@github/relative-time-element/dist": "assets/scripts/relative-time/" });
   eleventyConfig.addPassthroughCopy({ "node_modules/@zachleat/filter-container/filter-container.js": "assets/scripts/filter-container.js" });
-  eleventyConfig.addPassthroughCopy({ "node_modules/@lowlighter/matcha/dist/matcha.css": "assets/styles/matcha.css" });
 
   eleventyConfig.addTransform("parse", parseTransform);
-	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(highlightjs));
+  eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(highlightjs));
   eleventyConfig.on("afterBuild", async () => {
     // Create a Pagefind search index to work with
     const { index } = await pagefind.createIndex();
